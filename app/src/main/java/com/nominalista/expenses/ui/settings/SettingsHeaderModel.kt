fd896cbe6d5ef8af01fd5f3b3b70739f33ba622c
Nominalista <the.nominalista@gmail.com>
2018-07-20 13:12:31 +0200

    Remove unnecessary files.

--- app/src/main/java/com/nominalista/expenses/ui/settings/SettingsHeaderModel.kt
+++
@@ -1,3 +0,0 @@
-package com.nominalista.expenses.ui.settings
-
-class SettingsHeaderModel(val title: String) : SettingItemModel
--- app/src/main/java/com/nominalista/expenses/ui/settings/SummaryActionSettingItemModel.kt
+++
@@ -1,6 +0,0 @@
-package com.nominalista.expenses.ui.settings
-
-class SummaryActionSettingItemModel(val title: String, val summary: String) : SettingItemModel {
-
-    var click: (() -> Unit)? = null
-}
