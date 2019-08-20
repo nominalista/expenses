6abea6b5090ec2d9e4647e3630143336dc857b60
nominalista <the.nominalista@gmail.com>
2019-08-20 21:23:27 +0200

    Show full month name where possible

--- app/src/main/java/com/nominalista/expenses/util/DateUtils.kt
+++ app/src/main/java/com/nominalista/expenses/util/DateUtils.kt
@@ -2,6 +2,6 @@ package com.nominalista.expenses.util
 
 import org.threeten.bp.ZonedDateTime
 
-const val READABLE_DATE_FORMAT = "EEEE, d MMM"
+const val READABLE_DATE_FORMAT = "EEEE, d MMMM"
 
 fun getCurrentTimestamp() = ZonedDateTime.now().toInstant().toEpochMilli()
