42e8feceef2d5513224d8799eef304369b174e04
Subbramanian Lakshmanan <subbramanil@gmail.com>
2020-02-21 17:03:12 -0600

    feat: shows expenses for the chosen day in the calendar
    - Adds new ExpenseFragmentModel based off HomeFragmentModel which only has the required functions
    - Updates FilterExpensesUseCase to compare dates of the two expense items
    - Adds ExpenseHistoryAdapter with only the required functions to show expenses in rows
    - Uses simple Linear Layout to show the calendar view & list of expenses (recyclerview)
    
    **Note**: ExpenseFragmentModel is added as a redundant for a bit cleaner code. The same functionality can be achieved through few additional functions on "HomeFragmentModel". ExpenseFragmentModel can be removed by removing the comments in 'HomeFragmentModel' and replace usages of 'ExpenseFragmentModel'.

--- app/src/main/java/com/nominalista/expenses/expensehistory/presentation/ExpenseHistoryActivity.kt
+++ app/src/main/java/com/nominalista/expenses/expensehistory/presentation/ExpenseHistoryActivity.kt
@@ -1,4 +1,5 @@
 package com.nominalista.expenses.expensehistory.presentation
+
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
--- app/src/main/java/com/nominalista/expenses/home/domain/FilterExpensesUseCase.kt
+++ app/src/main/java/com/nominalista/expenses/home/domain/FilterExpensesUseCase.kt
@@ -3,6 +3,7 @@ package com.nominalista.expenses.home.domain
 import com.nominalista.expenses.data.model.Expense
 import com.nominalista.expenses.home.presentation.DateRange
 import com.nominalista.expenses.home.presentation.TagFilter
+import org.threeten.bp.LocalDate
 
 class FilterExpensesUseCase {
 
@@ -14,6 +15,15 @@ class FilterExpensesUseCase {
         return expenses.filter { expense ->
             dateRange.contains(expense.date) &&
                     tagFilter?.let { expense.tags.containsAll(it.tags) } ?: true
+        }
+    }
+
+    operator fun invoke(
+            expenses: List<Expense>,
+            date: LocalDate
+    ): List<Expense> {
+        return expenses.filter { expense ->
+            expense.date == date
         }
     }
 }
